f67333f1a5b40d536e011355cf5a8d2a8af8d87c
Gen-LJ <flashseeker1998@gmail.com>
2023-07-20 13:17:09 +0630

    upload percentage feature done

--- lib/provider/upload_post/blog_upload_provider.dart
+++ lib/provider/upload_post/blog_upload_provider.dart
@@ -11,7 +11,11 @@ class BlogUploadNotifier extends ChangeNotifier{
   void upload({required String title,required String body,required FormData data}) async{
     try{
       uploadUIState = UploadUILoading(0);
-        BlogUploadResponse blogUploadResponse = await _blogApiService.uploadPost(title: title, body: body, data: data);
+        BlogUploadResponse blogUploadResponse = await _blogApiService.uploadPost(title: title, body: body, data: data,
+        sendProgress: (int send,int size){
+          int progress =((send/size)*100).toInt();
+          uploadUIState=UploadUILoading(progress);
+        });
         uploadUIState = UploadUISuccess(blogUploadResponse);
     }
     catch(e){
